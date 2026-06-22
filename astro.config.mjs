6063abce4275402fd9ebdd26dc7ea8b75ac53345
Nina Wellenbrock <Nina.Wellenbrock@LM-AG.de>
2026-06-22 17:03:18 +0200

    Revert "feat: add seo opt"
    This reverts commit 6a46f9de4e88f239e801313b366d4e5e845fc919.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.tierpsychologie-nw.com',
-  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
   }
